e1ae6b4d6fc241b5a7b3e55e9e78de18689739e9
Bruno Pacheco <148828825+BrunoPS295@users.noreply.github.com>
2025-07-08 14:24:31 -0300

    ex9 dicionario

--- Curso em Video/Exercicios/ex9.py
+++ Curso em Video/Exercicios/ex9.py
@@ -1,4 +1,5 @@
 boletim = []
+
 while True:
     nome = str(input("Nome: "))
     n1 = float(input("Nota 1: "))
@@ -7,17 +8,20 @@ while True:
          print("Erro, as notas devem receber um valor de 0 a 10")
     else:
         media = (n1 + n2) /2
-        alunos = [nome, n1, n2, media]
+        alunos = {"nome":nome, "nota1":n1, "nota2":n2, "media":media}
         boletim.append(alunos)
         sn = str(input("Continuar? [S/N]: ")).lower()
         if sn == "n": break
+
 print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
 print(f"No. Nome {"Média":>10}" )
 print("---------------------------------------------------------------------")
+
 for i in range(len(boletim)):
-    dist = len(boletim[i][0]) - 13
-    print(f"{i}   {boletim[i][0]}  {boletim[i][3]:>{dist}.1f}")
+    dist = len(boletim[i]["nome"]) - 13
+    print(f"{i}   {boletim[i]["nome"]}  {boletim[i]["media"]:>{dist}.1f}")
 print("---------------------------------------------------------------------")
+
 while True:
     sn2 = int(input("Mostrar qual nota? [-1 para parar]"))
     if sn2 == -1: 
@@ -25,6 +29,6 @@ while True:
     elif sn2 > len(boletim) or sn2 < 0:
         print("Aluno não encontrado")
     else:
-        print(f"Notas de {boletim[sn2][0]} são [{boletim[sn2][1]:.1f}, {boletim[sn2][2]:.1f}]")
+        print(f"Notas de {boletim[sn2]["nome"]} são [{boletim[sn2]["nota1"]:.1f}, {boletim[sn2]["nota2"]:.1f}]")
         print("---------------------------------------------------------------------")
 print("<<< VOLTE SEMPRE >>>")
